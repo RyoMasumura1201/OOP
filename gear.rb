ad6cb3c6cb1c83776d97d010a1f970387513d5a0
ryo_of_the_ryo <ryomasumura1201@gmail.com>
2022-04-08 08:37:52 +0900

    wheelの完成

--- gear.rb
+++ gear.rb
@@ -3,7 +3,7 @@ class Gear
     def initialize(chainring, cog, rim, tire)
         @chainring = chainring
         @cog = cog
-        @wheel = Wheel.new(rim,tire)
+        @wheel = wheel
     end
 
     def ratio
@@ -15,12 +15,31 @@ class Gear
     end
 
     Wheel = Struct.new(:rim,:tire) do
-        def diameter
-            rim + (tire * 2)
-        end
+        
     end
     
 end
 
-puts Gear.new(52,11,26,1.5).ratio
-puts Gear.new(52,11,24,1.25).ratio
+class Wheel
+    attr_reader :rim, :tire
+
+    def initialize(rim,tire)
+        @rim = rim
+        @tire = tire
+    end
+
+    def diameter
+        rim + (tire * 2)
+    end
+
+    def circumference
+        diameter * Math::PI
+    end
+end
+
+@wheel = Wheel.new(26,1.5)
+
+puts @wheel.circumference
+
+puts Gear.new(52,11,@wheel).gear_inches
+puts Gear.new(52,11).ratio
